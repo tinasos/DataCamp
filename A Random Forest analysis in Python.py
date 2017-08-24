28ec9d1f18eb9dcd4c5c7acf5346d9c1bb7ce818
tinasos <tinasosiak@gmail.com>
2017-08-24 11:44:34 +0300

    Update A Random Forest analysis in Python.py

--- A Random Forest analysis in Python.py
+++ A Random Forest analysis in Python.py
@@ -10,3 +10,7 @@ my_forest = forest.fit(features_forest, target)
 
 # Print the score of the fitted random forest
 print(my_forest.score(features_forest, target))
+# Compute predictions on our test set features then print the length of the prediction vector
+test_features = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+pred_forest = my_forest.predict(test_features)
+print(len(pred_forest))
